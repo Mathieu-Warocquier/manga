060425edc525229871093698a14489d20fd0834c
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2023-04-10 18:38:52 +0200

    seed ok mais modifier type

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Launching the seed"
+puts "Destroying..."
+
+Anime.destroy_all
+
+puts "Creating users..."
+
+anime1 = Anime.create!(nom: "Dragon Ball", note: 17, titrevo: "ドラゴンボール", titrevf: "Dragon ball", dessinateur: "TORIYAMA Akira", editeur: "Glénat", genre: "Aventure, Fantastique", date: "Japon - 1985", image: "https://p4.wallpaperbetter.com/wallpaper/356/868/951/dragon-ball-dragon-ball-z-shenron-son-goku-wallpaper-preview.jpg")
+
+puts "seed is ok"
